4bafc0dd65d5fb3eca99daeb05773c5a7c6793a9
Jose Paiva <josepaiva@MacBook-Air-Jos.local>
2023-02-23 20:56:51 -0300

    Adjust page, Valid CNPJ, import ajax.

--- src/pages/validaCNPJ/validaCNPJ.jsx
+++ src/pages/validaCNPJ/validaCNPJ.jsx
@@ -2,20 +2,50 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../../components/NavBar";
 import InputMask from "react-input-mask";
 import axiosConfig from "../../axiosConfig";
+import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import Swal from "sweetalert2";
 
 const ValidaCNPJ = () => {
-  const [cnpj, setCnpj] = useState();
+  const preencha = "Preencha seu CNPJ";
+  const [disable, setDisable] = useState(true);
+  const [cnpj, setCNPJ] = useState();
+  const [button, setButton] = useState(preencha);
 
   useEffect(() => {
     // Atualiza o título do documento usando a API do browser
-    document.title = `Você clicou 1 vezes`;
+    document.title = `Clínica CNPJ`;
   });
 
   const validaCnpj = (elem) => {
-    axiosConfig.get("/Especialidades/Listar").then((response) => {
-      console.log(response);
-      // setCep(response.data);
-    });
+    localStorage.setItem('cnpj', elem.target.value);
+
+    if( elem.target.value != '' ){
+        axiosConfig.post("/Clinica/ValidaCNPJ?CNPJ="+elem.target.value)
+        .then((response) => {
+            if( response.data.statusCode === 200 && response.data.existente === false ){
+                setButton("Avançar");
+                setDisable(false);
+            }else{
+                setButton(preencha);
+                Swal.fire({
+                    icon: "warning",
+                    title: response.data.mensagem,
+                    showCancelButton: false,
+                    confirmButtonText: 'Ok',
+                });
+            }
+        })
+        .catch((err) =>{
+            setButton(preencha);
+            Swal.fire({
+                icon: "warning",
+                title: "Revise os campos!",
+                showCancelButton: false,
+                confirmButtonText: 'Ok',
+            });
+        })
+    }
   };
 
   return (
@@ -40,7 +70,7 @@ const ValidaCNPJ = () => {
 
         <div className="campo-cad">
           <InputMask
-            mask="00.000.000/0000-00"
+            mask="99.999.999/9999-99"
             onBlur={validaCnpj}
             name="cnpj"
             className="cnpj"
@@ -49,7 +79,10 @@ const ValidaCNPJ = () => {
           {/* <input type="text" className="cad-2" data-mask="00.000.000/0000-00" name="cnpj" id="cnpj" placeholder="Digite seu CNPJ" aria-label="Recipiente para nickname" aria-describedby="basic-addon2" /> */}
         </div>
 
-        <button className="btn-avancar">Avançar</button>
+        
+        <Button loading={true} className="btn-avancar" component={Link} to="/nome" disabled={disable}>
+            {button}
+        </Button>
       </div>
     </>
   );
